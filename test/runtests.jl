c1dd5f7410863640d39573eba2c03d62541b61bb
hannahilea <hannahilea@users.noreply.github.com>
2021-10-23 23:51:31 -0400

    fix test

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,7 +15,7 @@ include("agendas.jl")
     @test length(unique(meetings.name)) == 10
 
     results = search_agendas_for_content("9/1/2021", "9/30/2021", ["fluff"]);
-    @test nrow(results.meetings) == 16
+    @test nrow(results.meetings) == 13
     @test length(names(results.meetings)) == 6
     @test nrow(results.items) == 1
     @test isnothing(display_items_by_meeting(results.items))
